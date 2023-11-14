376a98c2af5c20e25c53e41fcfbfcdc64c1f67da
az <paveltretyakov.ru@gmail.com>
2023-11-14 20:32:14 +0300

    Deploy refactoring

--- start-stage.sh
+++ start-stage.sh
@@ -1,1 +1,23 @@
-docker compose -f docker-compose.yml -f docker-compose.stage.yml up -d --no-deps --build web
+#!/bin/bash
+
+# Подгружаем переменные окружения из файла .env
+if [[ -f .env ]]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo "Файл .env не найден."
+    exit 1
+fi
+
+# Проверяем установлен ли Certbot
+if ! command -v certbot &> /dev/null; then
+    echo "Certbot не установлен. Установите Certbot и попробуйте снова."
+    exit 1
+fi
+
+# Проверяем наличие сертификата для указанного домена
+if certbot certificates | grep -q $DOMAIN; then
+    echo "Сертификат для домена $DOMAIN существует."
+    docker compose -f docker-compose.yml -f docker-compose.web.yml -f docker-compose.nginx.yml up -d --no-deps --build
+else
+    echo "Сертификат для домена $DOMAIN не найден."
+fi
--- up-certbot.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker compose -f docker-compose.nginx.yml -f docker-compose.certbot.yml up -d --no-deps --build certbot
