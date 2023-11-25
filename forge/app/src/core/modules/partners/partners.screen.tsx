f01e513b7159c653ab625c1788353916175fcf80
Valeria Polovceva <curt.valeria@gmail.com>
2023-11-25 07:05:51 +0300

    fetching partners images

--- forge/app/src/core/modules/partners/partners.screen.tsx
+++ forge/app/src/core/modules/partners/partners.screen.tsx
@@ -4,35 +4,55 @@ import { NavigationStack } from 'core/types/navigation';
 
 import api from 'core/services/api';
 import { DrawerScreenProps } from '@react-navigation/drawer';
+import env, { envKyes } from 'core/services/env';
 
+interface ImageData {
+  name: string,
+  link: string
+}
 export const PartnersScreen = ({
   navigation,
 }: DrawerScreenProps<NavigationStack>) => {
   const [partners, setPartners] = useState([]);
+  const [images, setImages] = useState<ImageData[]>([]);
 
   useEffect(() => {
-    api.get('api/partners').then((result: any) => {
-      console.log('RESULT!', result);
-      setPartners(result.data);
-    });
+    try {
+      api.get('partners?populate=image').then((result: any) => {
+        setPartners(result.data || []);
+      })
+    } catch (e) {
+      console.log('Error on downloading partners', e)
+    };
   }, []);
 
+  useEffect(() => {
+    if (partners && partners.length) {
+      let imagesInfo: ImageData[] = [];
+
+      partners.map((data: any) => {
+        if (data && data.attributes) {
+          imagesInfo.push({ name: data.attributes.title, link: data.attributes.image.data.attributes.url })
+        }
+      })
+
+      setImages(imagesInfo)
+    }
+  }, [partners])
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {/* <Text>{t('welcome')}</Text> */}
-      <Image
-        style={{ width: '70%', height: '100%' }}
-        source={{ uri: require('assets/partners.png') }}
-      />
-
-      {/* <View style={styles.line}>
-        <Button
-          title="Go to parters"
-          onPress={() => navigation.navigate('Partners')}
-        />
+    <View style={{ alignItems: 'center', justifyContent: 'center', flexDirection: "row" }}>
+      <View style={{ margin: 10, flexWrap: 'wrap' }}>
+        {images && images.map((image) =>
+          <View key={`image-${image.name}`}
+            style={{ margin: 10 }}>
+            <Image
+              style={{ width: 300, height: 150 }}
+              source={{ uri: `${env[envKyes.apiHost]}${image.link}` }}
+            />
+          </View>
+        )}
       </View>
-
-      <Button title="Open drawer" onPress={() => navigation.openDrawer()} /> */}
     </View>
   );
 };
