c81043f8f08d3b1d2d1afe094976f074358910c2
Valeria Polovceva <curt.valeria@gmail.com>
2023-12-05 02:40:20 +0300

    стилизация страницы новостей

--- forge/app/src/core/modules/news/components/news-card.component.tsx
+++ forge/app/src/core/modules/news/components/news-card.component.tsx
@@ -1,11 +1,14 @@
 import { ScreenSize, useTheme, useWindowSize } from "core/providers/theme.provider";
 import env, { envKyes } from "core/services/env";
 import { useMemo } from "react"
-import { Image, StyleSheet, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
 import { NewsData } from "../news.types";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSetRecoilState } from "recoil";
 import { articleAtom } from "core/features/news/news.atoms";
+import { H4Text } from "core/components/text/h4.text";
+import { convertDateToString } from "core/helpers/date-string-converter";
+import { useTranslation } from "react-i18next";
 
 interface Props {
     data: NewsData
@@ -14,30 +17,97 @@ interface Props {
 export const NewsCard = ({ data }: Props) => {
     const { theme } = useTheme();
     const { sizeType } = useWindowSize();
-    const setArticle = useSetRecoilState(articleAtom);
 
+    const { t } = useTranslation();
+    const setArticle = useSetRecoilState(articleAtom);
 
     const style = useMemo(() => StyleSheet.create({
         wrapper: {
             display: 'flex',
-            flexDirection: sizeType !== ScreenSize.small ? 'row' : 'column',
             shadowColor: '#04060F',
             shadowOpacity: 0.1,
             shadowRadius: 20,
             margin: 20,
             backgroundColor: '#F6FAFD',
 
-            width: sizeType !== ScreenSize.small ? '90%' : 300,
-            maxWidth: '90%',
+            ...(sizeType !== ScreenSize.small ? {
+                flexDirection: 'row',
+                width: '90%',
+                maxWidth: '90%',
+            } : {
+                flexDirection: 'column',
+                width: '90%',
+                maxWidth: 300
+            })
         },
         infoWrapper: {
             display: 'flex',
             flexDirection: 'column',
+            justifyContent: 'space-between',
+            padding: 20,
+            width: '100%',
+
+            ...((sizeType === ScreenSize.small || !data.imageUrl) ? {
+                height: 'auto'
+            } : {
+                height: '100%',
+                paddingRight: 320,
+            })
         },
         imageWrapper: {
-            width: 300,
-            height: 200,
+            ...(sizeType !== ScreenSize.small ? {
+                width: 300,
+                height: '100%',
+            } : {
+                width: '100%',
+                height: 200,
+            })
         },
+        textWrapper: {
+            width: '100%',
+
+            ...((sizeType === ScreenSize.small || !data.imageUrl) && {
+                marginRight: 20,
+            })
+        },
+        title: {
+            flexWrap: 'wrap',
+        },
+        description: {
+            marginVertical: 10,
+            fontSize: 16,
+            fontWeight: '400',
+            lineHeight: 22,
+            letterSpacing: 0.2,
+        },
+        normalText: {
+            marginLeft: 5,
+            fontSize: 14,
+            fontWeight: '400',
+            lineHeight: 19.6,
+            letterSpacing: 0.2,
+        },
+        dateWrapper: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center'
+        },
+        bottomWrapper: {
+            marginTop: 20,
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        detailsRow: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            marginTop: 10,
+        },
+        detailsText: {
+            color: '#6842FF',
+        }
     }), [theme, sizeType]);
 
 
@@ -50,12 +120,26 @@ export const NewsCard = ({ data }: Props) => {
                         source={{ uri: `${env[envKyes.apiHost]}${data.imageUrl}` }} />
                 </View>}
             <View style={style.infoWrapper}>
-                <TouchableOpacity onPress={() => setArticle(data)}>
-                    <View>{data.title}</View>
-                </TouchableOpacity>
-
-                <View>{data.description}</View>
-                <View>{data.date}</View>
+                <View style={style.textWrapper}>
+                    <H4Text text={data.title.toUpperCase()} />
+                    <Text style={style.description}>{data.description}</Text>
+                </View>
+                <View style={style.bottomWrapper}>
+                    <View style={style.dateWrapper}>
+                        <Image style={{ width: 15, height: 15 }}
+                            source={require('assets/calendar-icon.png')} />
+                        <Text style={style.normalText}>{convertDateToString(data.date)}</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => setArticle(data)}>
+                        <View style={style.detailsRow}>
+                            <Text style={{ ...style.normalText, color: '#6842FF', marginRight: 5 }}>
+                                {t('info.details')}
+                            </Text>
+                            <Image style={{ width: 15, height: 15, tintColor: '#6842FF', paddingLeft: 10 }}
+                                source={require('assets/arrow-right.png')}></Image>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     )
