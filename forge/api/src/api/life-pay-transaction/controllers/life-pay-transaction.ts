41d5ea465d0729e51846e91004122e6680cded0e
az <paveltretyakov.ru@gmail.com>
2023-12-15 16:41:29 +0300

    Created life-pay transaction controller

--- forge/api/src/api/life-pay-transaction/controllers/life-pay-transaction.ts
+++ forge/api/src/api/life-pay-transaction/controllers/life-pay-transaction.ts
@@ -2,6 +2,14 @@
  * life-pay-transaction controller
  */
 
-import { factories } from '@strapi/strapi'
+// import { factories } from '@strapi/strapi'
+// import { Strapi, RequestContext } from '@strapi/strapi';
+import { Context } from 'koa';
 
-export default factories.createCoreController('api::life-pay-transaction.life-pay-transaction');
+// export default factories.createCoreController('api::life-pay-transaction.life-pay-transaction');
+export default {
+  async post(ctx: Context) {
+    console.log('TEEEST', ctx.request.body, ctx.state.user);
+    ctx.body = ctx.request.body;
+  },
+};
--- forge/api/src/api/life-pay-transaction/routes/life-pay-transaction.ts
+++ forge/api/src/api/life-pay-transaction/routes/life-pay-transaction.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::life-pay-transaction.life-pay-transaction');
+// export default factories.createCoreRouter('api::life-pay-transaction.life-pay-transaction');
+
+export default {
+  routes: [
+    {
+      method: 'POST',
+      path: '/life-pay-transaction',
+      handler: 'life-pay-transaction.post',
+    },
+  ],
+};
