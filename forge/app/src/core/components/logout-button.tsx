1e7de11d3d51075de3e2c545738901263fbfd406
az <paveltretyakov.ru@gmail.com>
2023-12-29 19:58:09 +0300

    Не проходит аутентификация#95

--- forge/app/src/core/components/logout-button.tsx
+++ forge/app/src/core/components/logout-button.tsx
@@ -1,45 +1,45 @@
-import { userAtom } from "core/features/users/users.atoms";
-import { saveToken } from "core/services/token";
-import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
-import { useSetRecoilState } from "recoil";
+import { tokenAtom, userAtom } from 'core/features/users/users.atoms';
+import { saveToken } from 'core/services/token';
+import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { useSetRecoilState } from 'recoil';
 
 export const LogoutButton = () => {
-    const setUser = useSetRecoilState(userAtom);
+  const setUser = useSetRecoilState(userAtom);
+  const setToken = useSetRecoilState(tokenAtom);
 
-    const clearStorage = async () => {
-        saveToken('');
-    };
+  const clearStorage = async () => {
+    saveToken('');
+  };
 
-    const logout = () => {
-        clearStorage().then(() => {
-            setUser(null)
-        }).catch((err) =>
-            console.log('Clear storage error', err)
-        );
-    };
+  const logout = () => {
+    clearStorage()
+      .then(() => {
+        setUser(null);
+        setToken('');
+      })
+      .catch((err) => console.log('Clear storage error', err));
+  };
 
-    return (
-        <View style={styles.wrapper}>
-            <TouchableOpacity onPress={() => logout()}>
-                <Image
-                    style={styles.icon}
-                    source={require('assets/logout-icon.png')} />
-            </TouchableOpacity>
-        </View>
-    )
+  return (
+    <View style={styles.wrapper}>
+      <TouchableOpacity onPress={() => logout()}>
+        <Image style={styles.icon} source={require('assets/logout-icon.png')} />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    icon: {
-        width: 21,
-        height: 22,
-        tintColor: '#bdbdbd'
-    },
+  wrapper: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    width: 21,
+    height: 22,
+    tintColor: '#bdbdbd',
+  },
 });
